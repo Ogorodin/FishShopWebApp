52079369d9f4e1b0641dbb038f5f5efda8a6c2d7
Siesti Trokot <igorvico@yahoo.com>
2019-03-08 15:02:44 +0100

    Initial commit

---
+++ src/main/java/org/ogorodin/FishShopWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.ogorodin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishShopWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishShopWebAppApplication.class, args);
+	}
+
+}
